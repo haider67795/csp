98e2f95a844d232b302a25014a570da77ab15e3c
haider67795 <haider67795@gmail.com>
2023-05-23 21:49:17 -0400

    Day 7 Upload V2
    Fixed some bugs in car game. Added "Win"/Loss mechanism to game. Added a life system and score keeper.

--- python/carGame.py
+++ python/carGame.py
@@ -4,13 +4,22 @@ import random
 
 pygame.init()
 carPic = pygame.image.load("media/mainCar.png")  # 1000x700
-car = pygame.transform.scale(carPic, (140, 240))
+car = pygame.transform.scale(carPic, (140, 220))
 
 oppPic = pygame.image.load("media/oppCar.png")
-oppCar = pygame.transform.scale(oppPic, (140, 240))
+oppCar = pygame.transform.scale(oppPic, (140, 220))
 
 oppPic2 = pygame.image.load("media/oppCar2.png")
-oppCar2 = pygame.transform.scale(oppPic2, (140, 240))
+oppCar2 = pygame.transform.scale(oppPic2, (140, 220))
+
+lanePic = pygame.image.load("media/lane.png")
+lane = pygame.Rect(490, 0, 5, 120)
+
+lane2Pic = pygame.image.load("media/lane.png")
+lane2 = pygame.Rect(785, 0, 5, 120)
+
+heartLoad = pygame.image.load("media/heart.png")
+heart = pygame.transform.scale(heartLoad, (50, 50))
 
 screen_width = 1280
 screen_height = 720
@@ -19,8 +28,8 @@ clock = pygame.time.Clock()
 carRect = pygame.Rect(
     screen_width // 2 - car.get_width() // 2,
     720 - car.get_height(),
-    200,
     140,
+    220,
 )
 
 
@@ -34,15 +43,12 @@ def move(rect):
     keys = pygame.key.get_pressed()
 
     if keys[pygame.K_a] and rect.x > 200:
-        rect.x -= 20
+        rect.x -= 10
 
     if keys[pygame.K_d] and rect.x < 1080 - car.get_width():
-        rect.x += 20
-    if keys[pygame.K_w] and rect.y > 240:
-        rect.y -= 20
+        rect.x += 10
 
-    if keys[pygame.K_s] and rect.y < 720 - car.get_height():
-        rect.y += 20
+    rect.x += random.randint(-5, 5)
 
 
 xchoices = [275, 570, 865]
@@ -61,17 +67,6 @@ oppRect2 = pygame.Rect(
 )
 
 
-def buildLanes():
-    lane1 = pygame.Rect(490, 0, 5, 36)
-    lane2 = pygame.Rect(785, 0, 5, 36)
-    for x in range(0, 11):
-        lane1.y = x * 36 * 2
-        pygame.draw.rect(screen, (255, 255, 255), lane1)
-    for x in range(0, 11):
-        lane2.y = x * 36 * 2
-        pygame.draw.rect(screen, (255, 255, 255), lane2)
-
-
 def checkCol():
     if carRect.colliderect(oppRect) or carRect.colliderect(oppRect2):
         return 1
@@ -84,7 +79,7 @@ running = True
 
 def moveOpp(opp):
     if opp.y < 720:
-        opp.y += random.randint(10, 20)
+        opp.y += 10
         return opp
     else:
         opp.x = xchoices[random.randint(0, 2)]
@@ -92,8 +87,32 @@ def moveOpp(opp):
         return opp
 
 
+def buildLanes():
+    lane.y += 10
+    if lane.y > 720:
+        lane.y = 0 - screen_height
+    lane2.y += 10
+    if lane2.y > 720:
+        lane2.y = 0 - screen_height
+    screen.blit(lanePic, (lane.x, lane.y - screen_height))
+    screen.blit(lanePic, (lane.x, lane.y))
+    screen.blit(lanePic, (lane.x, lane.y + screen_height))
+    screen.blit(lane2Pic, (lane2.x, lane2.y - screen_height))
+    screen.blit(lane2Pic, (lane2.x, lane2.y))
+    screen.blit(lane2Pic, (lane2.x, lane2.y + screen_height))
+
+
+def health(lives):
+    for x in range(lives):
+        screen.blit(heart, (50 * x, 0))
+
+
+count = 0
+font = pygame.font.SysFont("Arial", 20)
+lives = 3
 while running:
-    screen.fill("black")
+    collision_occurred = False
+    screen.fill("darkgreen")
 
     road()
 
@@ -103,27 +122,44 @@ while running:
         if event.type == QUIT:
             running = False
 
-    # if checkCol() == 1:
-    #     running = False
-
+    if checkCol() == 1:
+        carRect = pygame.Rect(
+            screen_width // 2 - car.get_width() // 2,
+            720 - car.get_height(),
+            140,
+            220,
+        )
+        oppRect = pygame.Rect(
+            xchoices[random.randint(0, 2)],
+            0 - oppCar.get_height(),
+            oppCar.get_width(),
+            oppCar.get_height(),
+        )
+        oppRect2 = pygame.Rect(
+            xchoices[random.randint(0, 2)],
+            0 - oppCar.get_height(),
+            oppCar.get_width(),
+            oppCar.get_height(),
+        )
+        lives -= 1
+    if lives == 0:
+        running = False
+    health(lives)
     move(carRect)
-    # while moveOpp(oppRect).x == moveOpp(oppRect2).x:
-    #     if moveOpp(oppRect2).y > 720:
-    #         moveOpp(oppRect2).x = xchoices[random.randint(0, 2)]
-    #     else:
-
     moveOpp(oppRect)
     moveOpp(oppRect2)
     if oppRect.colliderect(oppRect2):
-        oppRect2.x = xchoices[random.randint(0, 2)]
-        oppRect.y = 0 - oppCar2.get_height()
-        oppRect2.y = 0 - oppCar2.get_height()
-
+        while oppRect2.x == oppRect.x:
+            oppRect2.x = xchoices[random.randint(0, 2)]
+        oppRect.y = 0 - oppCar2.get_height() - random.randint(140, 200)
+        oppRect2.y = 0 - oppCar2.get_height() - random.randint(140, 200)
     screen.blit(car, (carRect.x, carRect.y))
     screen.blit(oppCar, (oppRect.x, oppRect.y))
     screen.blit(oppCar2, (oppRect2.x, oppRect2.y))
-
+    text = pygame.font.Font.render(font, "Score: " + str(count), 1, "white")
+    screen.blit(text, (screen_width // 2 - text.get_width(), 20))
     pygame.display.flip()
+    count += 1
     clock.tick(60)
-
+print("Your Highest Score Was " + str(count))
 pygame.quit()
